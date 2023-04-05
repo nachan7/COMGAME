695263ffb670907505da86e1c40406a46712f237
EC2 Default User <ec2-user@ip-172-31-11-206.ap-northeast-1.compute.internal>
2023-04-05 05:19:59 +0000

    [更新]admin/memberビューページ、コントロール

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -4,8 +4,18 @@ class Admin::MembersController < ApplicationController
   end
 
   def show
+    @member = Member.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @members = Member.all
+    @member = Member.find(params[:id])
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name,:email,:is_deleted)
   end
 end
