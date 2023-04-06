61149a28aa6721f526898e8c49eebd9e043364b5
EC2 Default User <ec2-user@ip-172-31-11-206.ap-northeast-1.compute.internal>
2023-04-06 05:24:06 +0000

    [add]レイアウト

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -2,6 +2,7 @@ class Public::MembersController < ApplicationController
 
   def index
     @members = Member.all
+
   end
 
   def show
@@ -13,16 +14,16 @@ class Public::MembersController < ApplicationController
   end
 
   def update
-    member = Member.find(params[:id])
-    member.update(member_params)
-    redirect_to member_path(current_member.id)
+    @member = Member.find(params[:id])
+    @member.update(member_params)
+    redirect_to member_path(@member.id)
   end
 
 
   private
 
   def member_params
-    params.require(:member).permit(:name,:email,:encrypted_password,:profile_image_url,:introduction,:gender,:playstyle,:gamerank,:voicechat,:playtime,:is_deleted)
+    params.require(:member).permit(:name,:email,:encrypted_password,:profile_image,:introduction,:gender,:playstyle,:gamerank,:voicechat,:playtime,:is_deleted)
   end
 
 end
