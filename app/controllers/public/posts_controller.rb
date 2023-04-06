c06efafa9dad2e48d87fc3c9b7324d65d8eda929
EC2 Default User <ec2-user@ip-172-31-11-206.ap-northeast-1.compute.internal>
2023-04-06 08:12:26 +0000

    [add]レイアウト変更

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -5,8 +5,8 @@ class Public::PostsController < ApplicationController
 
   def create
     post = Post.new(post_params)
-    post.save
-    redirect_to post_path(post.id)
+    post.save!
+    redirect_to posts_path
   end
 
   def index
@@ -22,9 +22,9 @@ class Public::PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-    redirect_to post_path(post.id)
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post.id)
   end
 
   def destroy
