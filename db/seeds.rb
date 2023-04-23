a27a0f5f54f7d86d4e77f983d7711cc07187caf1
EC2 Default User <ec2-user@ip-172-31-11-206.ap-northeast-1.compute.internal>
2023-04-23 11:26:45 +0000

    テストデータ挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,40 @@
 Admin.create!(
-    email: 'admin@admin',
-    password: 'comgame'
+    email: 'admin@example.com',
+    password: '2023comgame'
     )
-    
+
+categories = Category.create!(
+    [
+      {id: '1', name: 'PC'},
+      {id: '2', name: 'Switch'},
+      {id: '3', name: 'スマホ'},
+      {id: '4', name: 'PS5'},
+      {id: '5', name: 'PS4'},
+      {id: '6', name: 'Xbox'},
+      {id: '7', name: 'その他'}
+    ]
+    )
+
+members = Member.create!(
+    id: '1',
+    email: 'test@example.com',
+    name: 'テストユーザー',
+    password: 'testuser'
+    )
+
+members = Member.create!(
+    id: '2',
+    email: 'customer@example.com',
+    name: 'テスト会員',
+    password: 'customer',
+    gender: '非公開',
+    introduction: 'テスト会員といいます。よろしくお願いします。',
+    playstyle: 'エンジョイ',
+    gamerank: 'なし',
+    voicechat: 'なし',
+    playtime: '基本的に夜'
+    )
+
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
